8c5b32d36aba24ff87ae67b11f5a0bfa5a030fab
hyeoncheol kim <46736845+JHS91@users.noreply.github.com>
2019-07-22 14:53:20 -0500

    Add files via upload

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+import pickle
+import sys
+
+HOST = '10.21.76.82'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    conmsg = "Hi this is from client!"
+    s.sendall(conmsg.encode('utf-8'))
+    while True:
+        val = input("Type \'list\' to see list of text files: ")
+        if val == 'list':
+            val = val.encode('utf-8')
+            s.send(val)
+            break
+        else:
+            print("Invalid request, please enter correctly.\n")
+
+    data = s.recv(1024)
+    data_arr = pickle.loads(data)
+
+    for f in data_arr:
+        print(f)
+
+    val2 = input("\nEnter the file you want to stream with extension: ")
---
+++ sever.py
@@ -0,0 +1,28 @@
+import socket
+import os
+import pickle
+
+
+files = []
+for r, d, f in os.walk(os.path.normpath('C:\\Users\\compa\\OneDrive\\바탕 화면\\opentutorials')):
+    for file in f:
+        if '.txt' in file:
+            files.append(file)
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.bind(("10.21.76.82", 65432))
+mysock.listen(5)
+print ("Ready to serve")
+conn, addr = mysock.accept()
+data = conn.recv(1024)
+data = data.decode('utf-8')
+print(data)
+
+dt = conn.recv(1024)
+dt = dt.decode('utf-8')
+if dt == "list":
+    data = pickle.dumps(files)
+    conn.send(data)
+    for f in files:
+        print(f)
+
